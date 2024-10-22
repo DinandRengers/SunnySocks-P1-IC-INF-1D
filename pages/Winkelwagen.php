0e6d18b1efe8bc7e17a34b2912720f73300bc41e
DinandRengers <dinand.rengers@student.nhlstenden.com>
2024-10-22 14:07:46 +0200

    PHP functie (bijna) werkend hoef alleen er nog voor te zorgen dat data tijdelijk opgeslagen wordt.

--- pages/Winkelwagen.php
+++ pages/Winkelwagen.php
@@ -1,10 +1,34 @@
 <?php
     // PHP functie "aantal producten berekenen".
     $price = 12.50; // default prijs van het product
-    $quantity = isset($_POST['quantity']) ? (int)$_POST['quantity'] : 1; // aantal producten
-    $total_price = $price * $quantity; // totale prijs berekenen (Zonder BTW en Servicekosten)
-    $btw = $price * $quantity  / 121 * 21; // BTW berekenen "21%"
-    $total = $price * $quantity + $btw + 1.25; // Totale prijs berekenen (Met BTW en Servicekosten)
+
+    // Array voor de product quantity's
+    $quantity = [
+        'quantity1' => isset($_POST['quantity1']) ? (int)$_POST['quantity1'] : 1, // aantal producten
+        'quantity2' => isset($_POST['quantity2']) ? (int)$_POST['quantity2'] : 1, // aantal producten
+        'quantity3' => isset($_POST['quantity3']) ? (int)$_POST['quantity3'] : 1  // aantal producten
+    ];
+
+    // Haalt de data van $quantity uit elkaar en maakt er 3 variabelen van
+    $quantity1 = (int)$quantity['quantity1'];
+    $quantity2 = (int)$quantity['quantity2'];
+    $quantity3 = (int)$quantity['quantity3'];
+
+    // Berekeningen voor ieder productprijs
+    $price1 = ($price * $quantity1);
+    $price2 = ($price * $quantity2);
+    $price3 = ($price * $quantity3);
+
+    // test
+    echo var_dump($quantity);
+
+    // Pakt alle data van de array $quantity en voegt het bij elkaar
+    $totale_quantity = array_sum($quantity);
+    
+    // Berekeningen van de btw/prijzen
+    $total_price = $price * $totale_quantity; // totale prijs berekenen (Zonder BTW en Servicekosten)
+    $btw = $price * $totale_quantity  / 121 * 21; // BTW berekenen "21%"
+    $total = $price * $totale_quantity + $btw + 1.25; // Totale prijs berekenen (Met BTW en Servicekosten)
 ?>
 
 <!DOCTYPE html>
@@ -50,14 +74,14 @@
                             <p>Sunny Socks</p>
                             <p>Uni-Socks Groen</p>
                             <label for="quantity">Aantal:</label>
-                            <input class="Aantal" type="number" name="quantity" value="<?php echo $quantity; ?>" min="1" max="99">
+                            <input class="Aantal" type="number" name="quantity1" value="<?php echo $quantity['quantity1']; ?>" min="1" max="99">
                             <p>Prijs:<?php echo ' €', number_format ($price, 2, ',', '.'); ?></p>
                             <button type="submit">Toepassen</button>
                         </div>
                     </form>
                 </div>
                 <div class="wwproduct1prijs">
-                    <p>Prijs: €<?php echo number_format($total_price, 2, ',', '.'); ?></p>
+                    <p>Prijs: €<?php echo number_format($price1, 2, ',', '.'); ?></p>
                 </div> 
                 <div class="wwproduct2">
                     <img class="cancelbutton" src="../content/cancel_icon.png" alt="cancel_icon">
@@ -69,14 +93,14 @@
                             <p>Sunny Socks</p>
                             <p>Uni-Socks Rood</p>
                             <label for="quantity">Aantal:</label>
-                            <input class="Aantal" type="number" name="quantity" value="<?php echo $quantity; ?>" min="1" max="99">
+                            <input class="Aantal" type="number" name="quantity2" value="<?php echo $quantity['quantity2']; ?>" min="1" max="99">
                             <p>Prijs:<?php echo ' €', number_format ($price, 2, ',', '.'); ?></p>
                             <button type="submit">Toepassen</button>
                         </div>
                     </form>
                 </div>
                     <div class="wwproduct2prijs">
-                        <p>Prijs: €<?php echo number_format($total_price, 2, ',', '.'); ?></p>
+                        <p>Prijs: €<?php echo number_format($price2, 2, ',', '.'); ?></p>
                     </div>
                 <div class="wwproduct3">
                     <img class="cancelbutton" src="../content/cancel_icon.png" alt="cancel_icon">
@@ -88,24 +112,28 @@
                             <p>Sunny Socks</p>
                             <p>Gestreepde socks Geel</p>
                             <label for="quantity">Aantal:</label>
-                            <input class="Aantal" type="number" name="quantity" value="<?php echo $quantity; ?>" min="1" max="99">
+                            <input class="Aantal" type="number" name="quantity3" value="<?php echo $quantity['quantity3']; ?>" min="1" max="99">
                             <p>Prijs:<?php echo ' €', number_format ($price, 2, ',', '.'); ?></p>
                             <button type="submit">Toepassen</button>
                         </div>
                     </form>
                 </div>
                     <div class="wwproduct3prijs">
-                        <p>Prijs: €<?php echo number_format($total_price, 2, ',', '.'); ?></p>
+                        <p>Prijs: €<?php echo number_format($price3, 2, ',', '.'); ?></p>
                     </div>
                 <div class="wwinfo">
                     <img src="../content/sunny_logos_slogan_blue.png" alt="sunny_logos_slogan_blue">
                 </div>
                     <div class="totaalprijs">
-                        <p>Prijs: €<?php echo number_format($total_price, 2, ',', '.'); ?></p>
-                        <p>BTW: €<?php echo number_format($btw, 2, ',', '.'); ?></p>
-                        <p>Service: €1,25</p>
-                        <p><br></p>
-                        <p>Totaal: €<?php echo number_format($total, 2, ',', '.'); ?></p>
+                        <ul>
+                            <li><p>Prijs: €<?php echo number_format($total_price, 2, ',', '.'); ?></p></li>
+                            <li><p>BTW: €<?php echo number_format($btw, 2, ',', '.'); ?></p></li>
+                            <li><p>Service: €1,25</p></li>
+                            <hr>
+                            <div class="wwtotaal">
+                            <li><p>Totaal: €<?php echo number_format($total, 2, ',', '.'); ?></p></li>
+                            </div>
+                        </ul>
                     </div>
         </section>
         <section class="winkelwagenbanner"></section>
