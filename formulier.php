24f4af6d58e4dafa348915464e2f984853bbb02e
SilvioFeio <silvio.antonio.feio@student.nhlstenden.com>
2024-10-24 10:12:55 +0200

    Update formulier.php

--- formulier.php
+++ formulier.php
@@ -1,11 +1,15 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST");
-    {
-        //valideert de input
-        $voornaam = filter_input(INPUT_POST, "voornaam");
-        $achternaam = filter_input(INPUT_POST, "achternaam");
-        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-        $bericht = filter_input(INPUT_POST, "bericht");
-        header("Location: pages/contact.html");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $voornaam = filter_input(INPUT_POST, "voornaam", FILTER_SANITIZE_STRING);
+    $achternaam = filter_input(INPUT_POST, "achternaam", FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+    $bericht = filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_STRING);
+
+    if ($voornaam && $achternaam && $email && $bericht) {
+    } else {
+        
+        header("Location: /contact.html?"); 
     }
-?>
+
+}
