ab91d2ceb33572f77868038d0b648d225abe9033
RobbinSkippy <robbin.schipper@student.nhlstenden.com>
2024-10-25 11:09:07 +0200

    search functie zoektermen aangepast. formulier werkend gemaakt met verbeterde filter

--- functies/formulier.php
+++ functies/formulier.php
@@ -1,18 +1,19 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $voornaam = filter_input(INPUT_POST, "voornaam", FILTER_SANITIZE_STRING);
-    $achternaam = filter_input(INPUT_POST, "achternaam", FILTER_SANITIZE_STRING);
+if($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $voornaam = filter_input(INPUT_POST, "voornaam", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $achternaam = filter_input(INPUT_POST, "achternaam", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    $bericht = filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_STRING);
+    $bericht = filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    if($voornaam && $achternaam && $email && $bericht) 
+    // Controleer of de verplichte velden zijn ingevuld en of het emailadres geldig is
+    if ($voornaam && $achternaam && $email && $bericht)
     {
         header("Location: ../pages/home.php");
-    } 
+    }
     else
     {
         header("Location: ../pages/contact.html");
     }
-
 }
+?>
--- functies/search.php
+++ functies/search.php
@@ -1,45 +1,21 @@
 <?php
 
-$zoekterm = filter_input(INPUT_POST, "zoeken");
+$zoekterm = strtolower(filter_input(INPUT_POST, "zoeken"));
 
-// zoek termen hier in de array
+$rood = array("rood", "strepen", "classic stripes", "stripes", "rode sok", "rode sokken", "strepen rood");
+$groen = array("groen", "uni color", "classic uni color", "uni", "groene sok", "groene sokken", "sok groen");
 
-$collectie = array("collectie");
-$overOns = array("over ons", "geschiedenis");
-$blog = array("blog");
-$contact = array("contact");
-$winkelwagen = array("winkelwagen");
-$startpagina = array("startpagina", "home");
-
-// zoekfunctie die kijkt in de array
-
-if(in_array($zoekterm, $collectie))
+if(in_array($zoekterm, $rood))
 {
-    header("Location: pages/collectie.html");
+    header("Location: ../pages/product_pagina_strepren_rood.html");
 }
-elseif(in_array($zoekterm, $overOns))
+elseif(in_array($zoekterm, $groen))
 {
-    header("Location: pages/overons.html");
-}
-elseif(in_array($zoekterm, $blog))
-{
-    header("Location: pages/blog.html");
-}
-elseif(in_array($zoekterm, $contact))
-{
-    header("Location: pages/contact.html");
-}
-elseif(in_array($zoekterm, $winkelwagen))
-{
-    header("Location: pages/winkelwagen.html");
-}
-elseif(in_array($zoekterm, $startpagina))
-{
-    header("Location: index.html");
+    header("Location: ../pages/product_pagina_groen.html");
 }
 else
 {
-    header("Location: index.html");
+    header("Location: ../pages/home.php");
 }
 
 ?>
